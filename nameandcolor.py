b6ea1c5a97a448f30cfb9b018c7f4a57976aca23
TiaanVenter <72299253+TiaanVenter@users.noreply.github.com>
2020-10-03 11:29:42 +0200

    Add files via upload

---
+++ nameandcolor.py
@@ -0,0 +1,3 @@
+name = input("What's your name? ")
+color = input("What color do you like? ")
+print(name + ' likes ' + color)
---
+++ poundtokilo.py
@@ -0,0 +1,3 @@
+weight_pounds = input("How much do you weigh in pounds? ")
+kilo = float(weight_pounds) * 0.453592
+print("You weigh " + str(kilo) + " kilograms.")
