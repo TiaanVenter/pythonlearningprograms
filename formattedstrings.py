7fbfa9ac2b2e942f412900d0c7976e2ba4c84fa3
TiaanVenter <72299253+TiaanVenter@users.noreply.github.com>
2020-10-03 15:07:22 +0200

    Add files via upload

---
+++ formattedstrings.py
@@ -0,0 +1,6 @@
+first = 'Tiaan'
+last = "Venter"
+nonformattedmessage = first + ' [' + last + '] is a coder'
+formattedmsg = f'{first} [{last}] is a coder'
+print(nonformattedmessage) 
+print(formattedmsg)
---
+++ len_example_purpose.py
@@ -0,0 +1,9 @@
+#this program takes a variable and prints out the length of the string (or int?) into the terminal/window.
+#limit length of input!
+course = 'py4beginners'
+strlen = len(course)
+if strlen > 20 :
+    print('Too much!')
+else:
+    print("Less than 20!")
+    
