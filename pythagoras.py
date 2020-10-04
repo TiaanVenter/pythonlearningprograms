2f2c7e68ff61434c010b42fd83d9c3ec9c6a536a
TiaanVenter <72299253+TiaanVenter@users.noreply.github.com>
2020-10-04 15:45:59 +0200

    Add files via upload

---
+++ pythagoras.py
@@ -0,0 +1,37 @@
+from math import sqrt
+
+print('Pythagorean theorem calculator! Calculate your triangle sides.')
+print('Assume the sides are a, b, c and c is the hypotenuse (the side opposite the right angle')
+formula = input('Which side (a, b, c) do you wish to calculate? side> ')
+
+if formula == 'c':
+	side_a = float(input('Input the length of side a: '))
+	side_b = float(input('Input the length of side b: '))
+
+	side_c = sqrt(side_a * side_a + side_b * side_b)
+	
+	print('The length of side c is: ' )
+	print(side_c)
+
+elif formula == 'a':
+    side_b = float(input('Input the length of side b: '))
+    side_c = float(input('Input the length of side c: '))
+    
+    side_a = sqrt((side_c * side_c) - (side_b * side_b))
+    
+    print('The length of side a is' )
+    print(side_a)
+
+elif formula == 'b':
+    side_a = float(input('Input the length of side a: '))
+    side_b = float(input('Input the length of side c: '))
+        
+    side_c = sqrt(side_c * side_c - side_a * side_a)
+    
+    print('The length of side b is')
+    print(side_c)
+
+else:
+	print('Please select a side between a, b, c')
+
+    #comment to change hash(?). This was stolen from somewhere. My apologies for not crediting, but a simple google search for 'Python Pythagoras' shouldd bring you to the same program.
